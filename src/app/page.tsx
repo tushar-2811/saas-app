299bd9cdc8a23bbfbe778a6c4f7c2573373ce630
tushar-2811 <tusharrawat52@gmail.com>
2025-03-08 02:20:20 +0530

    add introduction section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import Hero from "@/sections/Hero";
+import Introduction from "@/sections/Introduction";
 import LogoTicker from "@/sections/LogoTicker";
 import Navbar from "@/sections/Navbar";
 
@@ -7,5 +8,6 @@ export default function Home() {
       <Navbar/>
       <Hero/>
       <LogoTicker/>
+      <Introduction/>
     </>
 }
